6a61d2f72c0b4786977e265279ab775a4033fbb0
zajebs <richy.mister@gmail.com>
2024-05-25 17:35:54 +0300

    indexcount

--- routes/index.py
+++ routes/index.py
@@ -1,9 +1,24 @@
 from flask import Blueprint, render_template, request
+from lib.database import get_db_connection
 
 index_bp = Blueprint('index', __name__)
 
 @index_bp.route('/')
 def index():
-    if request.method == 'GET':
-        update = request.args.get('update', '')
-    return render_template('index.html', update=update)
+    update = request.args.get('update', '')
+
+    conn = get_db_connection()
+    cur = conn.cursor()
+
+    if update:
+        like_clause = "name ILIKE %s"
+        cur.execute(f"SELECT COUNT(*) FROM products WHERE {like_clause}", ('%' + update + '%',))
+    else:
+        cur.execute("SELECT COUNT(*) FROM products")
+
+    total_count = cur.fetchone()[0]
+
+    cur.close()
+    conn.close()
+
+    return render_template('index.html', update=update, total_count=total_count)
