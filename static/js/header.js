ae22a5a1852cc789b4c2b7adc5cdb7ae533a6f32
zajebs <richy.mister@gmail.com>
2024-05-27 08:19:51 +0300

    header

--- static/js/header.js
+++ static/js/header.js
@@ -23,3 +23,12 @@ document.addEventListener('DOMContentLoaded', function() {
             });
     }
 });
+
+function toggleMenu() {
+    var menu = document.getElementById('mobile-menu');
+    if (menu.classList.contains('hidden')) {
+        menu.classList.remove('hidden');
+    } else {
+        menu.classList.add('hidden');
+    }
+}
--- static/js/script.js
+++ static/js/script.js
@@ -274,13 +274,4 @@ window.onscroll = function() {
 	}
 };
 
-function toggleMenu() {
-    var menu = document.getElementById('mobile-menu');
-    if (menu.classList.contains('hidden')) {
-        menu.classList.remove('hidden');
-    } else {
-        menu.classList.add('hidden');
-    }
-}
-
 resetAndLoadProducts();
